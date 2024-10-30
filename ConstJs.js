7edae88b458f0b1f852d976ea1da1514da52d698
SteppyCoyote111 <92569647+SteppyCoyote111@users.noreply.github.com>
2024-10-30 12:22:03 +0300

    Update ConstJs.js

--- ConstJs.js
+++ ConstJs.js
@@ -3,3 +3,4 @@ window.myArray = [1, 2, 3, 4, 5];
 window.myFunction = function() {
     console.log("This is a function from an external script.");
 };
+window.dict = { Корм_рыбам: "Корм рыбам", Age: 23, Job: "Freelancer", Skills: "JavaScript" }
